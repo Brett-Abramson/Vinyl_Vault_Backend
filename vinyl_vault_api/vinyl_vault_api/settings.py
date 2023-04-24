56318a8e40cb458c03d62f713526686cc1c15271
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-24 09:22:49 -0500

    added .env file and installed django_environ

--- vinyl_vault_api/vinyl_vault_api/settings.py
+++ vinyl_vault_api/vinyl_vault_api/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c&8zkg#8kb#2z02m1w47zk@&a_lez40(z0xcr)_gky#2q8knr$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +84,7 @@ WSGI_APPLICATION = 'vinyl_vault_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_api',
+        'NAME': env('DATABASE_NAME'),
         'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost'

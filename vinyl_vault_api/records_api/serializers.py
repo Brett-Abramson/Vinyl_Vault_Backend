3edb752390453a241a31b2933249b60e2e3901db
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-28 15:36:20 -0500

    updated models, new optional fields, renamed year_released -> release_date.

--- vinyl_vault_api/records_api/serializers.py
+++ vinyl_vault_api/records_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Album
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        fields = ("id", "artist", "title", "year_released")
+        fields = ("id", "artist", "title", "tracks", "release_date", "artwork", "spotify_id")
